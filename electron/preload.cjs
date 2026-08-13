621b56f428e57d3349ad8be222853cfca2690bfa
BlackMamba <neocyber1@gmail.com>
2026-08-13 00:46:45 -0600

    expose visual anchor controls to the deck

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -10,6 +10,9 @@ contextBridge.exposeInMainWorld("shipShellBrowser", {
   forward: () => ipcRenderer.invoke("browser:forward"),
   reload: () => ipcRenderer.invoke("browser:reload"),
   getPageContext: () => ipcRenderer.invoke("browser:get-context", { includeVisual: true }),
+  setAnnotationMode: (mode) => ipcRenderer.invoke("browser:set-annotation-mode", mode),
+  clearAnnotations: () => ipcRenderer.invoke("browser:clear-annotations"),
+  getAnnotations: () => ipcRenderer.invoke("browser:get-annotations"),
   setBounds: (bounds) => ipcRenderer.send("browser:set-bounds", bounds),
   setVisible: (visible) => ipcRenderer.send("browser:set-visible", visible),
   onState: (listener) => {
