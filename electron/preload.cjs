6c943bfa1956a9a0b2c14a90405692e2443439cf
BlackMamba <neocyber1@gmail.com>
2026-08-12 21:33:48 -0600

    expose page context to command deck

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -9,6 +9,7 @@ contextBridge.exposeInMainWorld("shipShellBrowser", {
   back: () => ipcRenderer.invoke("browser:back"),
   forward: () => ipcRenderer.invoke("browser:forward"),
   reload: () => ipcRenderer.invoke("browser:reload"),
+  getPageContext: () => ipcRenderer.invoke("browser:get-context"),
   setBounds: (bounds) => ipcRenderer.send("browser:set-bounds", bounds),
   setVisible: (visible) => ipcRenderer.send("browser:set-visible", visible),
   onState: (listener) => {
