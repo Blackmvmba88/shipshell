d9c33f0222e72e7a6e71fc3a0547ba134a80fbff
BlackMamba <neocyber1@gmail.com>
2026-08-16 04:28:04 -0600

    Wire professional theme tokens into agent modules

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { AnnotationDock } from "./AnnotationDock";
 import "./styles.css";
+import "./integration-tokens.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
